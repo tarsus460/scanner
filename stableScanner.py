1711abaf5f332daa9ea1c27b2bcf46d34eec197c
tarsus460 <32818516+tarsus460@users.noreply.github.com>
2020-11-07 08:18:10 -0600

    Update stableScanner.py

--- stableScanner.py
+++ stableScanner.py
@@ -8,14 +8,6 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from pathlib import Path
 
-def fileList():
-        n = 1
-        cdate = datetime.date.today()
-        for filename in os.listdir(pathy):
-                m = str(cdate) + '#' + str(n) + '.pdf'
-                shutil.copy( pathy / filename, pathee /str(m))
-                n = n + 1
-
 pathy = Path(r'C:\Users\phrogg\Documents\Pathy')         # YOUR PATH WILL BE DIFFERENT
 pathee = Path(r'C:\Users\phrogg\Documents\Pathee')       # YOUR PATH WILL BE DIFFERENT
 
@@ -25,10 +17,30 @@ profile = webdriver.FirefoxProfile(r'C:\Users\phrogg\AppData\Roaming\Mozilla\Fir
 browser = webdriver.Firefox(profile,options=options)
 browser.get('http://192.168.1.65/#hId-webscanPage') # YOUR IP WILL BE DIFFERENT
 
+currentList = os.listdir(Path.cwd())
+config = 'config.txt'
+print('##############################')
+print('##### StableScanner v1.2 #####')
+print('##############################')
+def nameLoop():
+    nameQuery = input('Query: Would you like to name your batch of documents today? ')
+    global cdate
+    if nameQuery == 'y':
+        cdate = input('and what would that name be? ')
+        configCheck()
+    elif nameQuery == 'n':
+        cdate = datetime.date.today()
+        configCheck()
+
+def fileList():
+        n = 1
+        #cdate = datetime.date.today()
+        for filename in os.listdir(pathy):
+                m = str(cdate) + '#' + str(n) + '.pdf'
+                shutil.copy( pathy / filename, pathee /str(m))
+                n = n + 1
+
 def scanLoop():
-    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    print('!!!!!!!WAIT FOR FIREFOX TO LOAD THE PAGE BEFORE GIVING INPUT!!!!!!!')
-    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
     scanning = input('would you like to scan a document? Y/N ')
     while scanning == 'y':
         fileType = browser.find_element_by_xpath('/html/body/div[5]/div[2]/div/form/div[1]/div[1]/div/div[1]/div/select/option[1]')
@@ -42,5 +54,19 @@ def scanLoop():
     else:
         print('i\m sorry, that won\'t work')
         scanLoop()
-
-scanLoop()
+def configCheck():
+    print(currentList,'\n\n')
+    if config in currentList:
+        scanLoop()
+    else:
+        print('no config found, creating...\n\n')
+        p = open('config.txt','a')
+        p.write('This is the Program\'s configuration\n\n')
+        IP = input('what is your printer\'s IP address?')
+        p.write('IP Address: ' + str(IP) + '\n\n')
+        global downloadFolder
+        downloadFolder = input('What is the download folder for Firefox? ')
+        p.write('DownloadPath = ' + downloadFolder)
+        p.close()
+        scanLoop()
+nameLoop()
